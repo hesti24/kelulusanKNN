4f83375e9cb1d6ba780da78ee6cb54c75f39ba98
hesti24 <121180101+hesti24@users.noreply.github.com>
2022-12-23 20:21:45 +0700

    Update stream-mahasiswa.py

--- stream-mahasiswa.py
+++ stream-mahasiswa.py
@@ -11,15 +11,15 @@ st.title('Prediksi Kelulusan Mahasiswa STT Wastukancana')
 col1, col2 = st.columns(2)
 
 with col1 :
-    IPS1 = st.text_input ('masukan IPS1')
-    IPS2 = st.text_input('masukan IPS2')
-    IPS3 = st.text_input('masukan IPS3')
-    IPS4 = st.text_input('masukan IPS4')
+    IPS1 = st.number_input ('masukan IPS1')
+    IPS2 = st.number_input('masukan IPS2')
+    IPS3 = st.number_input('masukan IPS3')
+    IPS4 = st.number_input('masukan IPS4')
 with col2 :    
-    IPS5 = st.text_input('masukan IPS5')
-    IPS6 = st.text_input('masukan IPS6')
-    IPS7 = st.text_input('masukan IPS7')
-    IPS8 = st.text_input('masukan IPS8')
+    IPS5 = st.number_input('masukan IPS5')
+    IPS6 = st.number_input('masukan IPS6')
+    IPS7 = st.number_input('masukan IPS7')
+    IPS8 = st.number_input('masukan IPS8')
 
 # Code untuk Prediksi
 ket_Kelulusan = ''
@@ -28,7 +28,7 @@ ket_Kelulusan = ''
 if st.button('Test Prediksi Kelulusan') :
     ket_prediction = mahasiswa_model.predict([[ IPS1, IPS2, IPS3, IPS4, IPS5, IPS6 , IPS7 , IPS8]])
 
-    if(ket_prediction[0.]==1.):
+    if(ket_prediction[0]==1):
         ket_Kelulusan = 'Mahasiswa Lulus'
     else : 
         ket_Kelulusan = 'Mahasiswa tidak lulus'
